fd4836f013632c5be12da3de6b7b44ffcabc4b36
Guilherme-Tavares <guilhermemaricatotavares@gmail.com>
2026-03-11 17:19:00 -0400

    Project setup with Express and TypeScript

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Home
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/home.html'));
+});
+
+// About
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/about.html'));
+});
+
+// Contact
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/contact.html'));
+});
+
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+
+    res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Web server running in http://localhost:${port}`);
+});
